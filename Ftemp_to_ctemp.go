5e0de95050c41f13cccbb8cdc14c739e1f06c2ba
Loweski <lengvang9000@gmail.com>
2015-03-02 11:36:06 -0800

    Create Ftemp_to_ctemp.go

---
+++ Ftemp_to_ctemp.go
@@ -0,0 +1,17 @@
+// golanghw project main.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+/*	var ftemp int
+	var ctemp int
+	fmt.Println("Enter your Fahrenheit temperature:")
+	fmt.Scanf("%d", &ftemp)
+	ctemp = (ftemp - 32) * 5 / 9
+	fmt.Println(ftemp, " in Fahrenheit is, in Celsius,", ctemp)*/
+	
+	
+}
