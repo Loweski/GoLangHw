2b7bdc0c7d78cbb510eae821b911b738503c02c0
Loweski <lengvang9000@gmail.com>
2015-03-02 11:55:59 -0800

    Create find_min.go

---
+++ find_min.go
@@ -0,0 +1,19 @@
+// golanghw project main.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := []int{48, 96, 86, 68, 57, 82, 63, 70, 37, 34, 83, 27, 19, 97, 9, 17}
+
+	min := x[0]
+	for _, curr := range x {
+		if min > curr {
+			min = curr
+		}
+	}
+
+	fmt.Println(min)
+}
